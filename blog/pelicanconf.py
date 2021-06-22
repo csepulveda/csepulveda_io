02cb4b88828b9daf501f66328265791fe75a2ea7
Cesar Sepulveda <cesar.sepulveda@xapo.com>
2021-06-22 18:06:19 -0400

    test theme

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -25,10 +25,11 @@ AUTHOR_FEED_RSS = None
 #          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/csepulveda/'),
-         ('Instagram', 'https://instagram.com/csepulvedab/'),)
-
-DEFAULT_PAGINATION = 10
+SOCIAL = (
+    ('linkedin', 'https://www.linkedin.com/in/csepulvedab/'),
+    ('github', 'https://github.com/csepulveda'),
+    ('instagram', 'https://instagram.com/csepulvedab/'),
+)
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -37,5 +38,7 @@ THEME = 'themes/Peli-Kiera'
 
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['readtime', 'neighbors']
+SUMMARY_MAX_LENGTH = 100
+DEFAULT_PAGINATION = 10
 
 
